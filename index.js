d34aac7688099718697452e814107352943075a6
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-04-01 19:32:34 +0800

    0.5.14

---
+++ index.js
@@ -0,0 +1,12 @@
+// const path = require('path');
+// const pkg = require('./package.json');
+
+// const obj = Object.create(null);
+// obj.version = pkg.version;
+// obj.path = path.join(__dirname, 'dist');
+
+
+// // obj.files = pkg.files;
+
+// pkg = null;
+module.exports = __dirname;
